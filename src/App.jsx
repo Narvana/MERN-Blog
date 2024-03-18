86d078f90d65c26f85b9077e3cfd5076ba8b77ee
Narvana <adhikarijagdish3@gmail.com>
2024-03-18 11:10:54 +0530

    Create Header Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import {About,Home,Signin,Signup,Dashboard,Projects} from './pages/Index'
+import {Header,Footer} from './component/Index'
 
 function App() {
 
   return (
     <BrowserRouter>
+    <Header/>
      <Routes>
       <Route path="/" element={<Home />}/>
       <Route path="/about" element={<About />}/>
@@ -13,6 +15,7 @@ function App() {
       <Route path="/dashboard" element={<Dashboard/>}/>
       <Route path="/project" element={<Projects />}/>
      </Routes>
+     <Footer/>
     </BrowserRouter>
   )
 }

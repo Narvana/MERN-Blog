2e21422dbfcafaf4655d94ae305545ee1003a5f5
Narvana <adhikarijagdish3@gmail.com>
2024-03-18 14:20:41 +0530

    Create Users Model

---
+++ api/model/userMod.js
@@ -0,0 +1,38 @@
+const mongoose=require('mongoose')
+// const 
+
+const userSchema=new mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        trim:true,
+        unique:true
+    },
+    email:{
+        type:String,
+        required:true,
+        trim:true,
+        unique:true,
+        validate:{
+            validator(value){
+                if(!validator.isEmail(value)){
+                    throw new Error("Write a Valid Email")
+                }    
+            }
+        }
+    },
+    password:{
+        type:String,
+        require:true,
+        trim:true,
+        // validate: {
+        //     validator: (value)=>{
+        //       return /^\d{8}$/.test(value);
+        //     },
+        //     message: 'Invalid password. Please enter a valid 8-character password.',
+        //   },
+    },
+})
+
+const Users=new mongoose.model('Users',userSchema)
+module.exports=Users;

6f16c0e991aa8d8fabded78084540ae4ea81c457
Narvana <adhikarijagdish3@gmail.com>
2024-03-18 11:52:54 +0530

    Create API

---
+++ api/index.js
@@ -0,0 +1,13 @@
+require('dotenv').config
+const express=require('express');
+// const 
+const app=express();
+const port=process.env.PORT || 5000
+
+app.get('/',(req,res)=>{
+    res.send('Hello')
+})
+
+app.listen(port,()=>{
+    console.log(`Connected to port ${port}`);
+})

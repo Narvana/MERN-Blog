be9b0c0cbe7bc5af11bc796657ead9ef70b0fb6d
Narvana <adhikarijagdish3@gmail.com>
2024-03-26 10:31:08 +0530

    Update Home Page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
-// import { useDispatch, useSelector } from 'react-redux'
-// import { signInStart ,signInSuccess ,signInFailure } from '../redux/user/userSlice'
+import { useDispatch, useSelector } from 'react-redux'
+import { signInStart ,signInSuccess ,signInFailure } from '../redux/user/userSlice'
 
 // import { Welcome } from './Signin'
 
 function Home() {
-
+  const {currentUser}=useSelector(state=>state.user)
+  // const dispatch=useDispatch()
+  console.log(currentUser);
   return (
     <div>
-      Welcome to the Web's Blog
+      Welcome to the Web's Blog {currentUser && <div>{currentUser.rest.username}</div>}
     </div>
   )
 }

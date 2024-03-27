9e968088db9386ec07136e99550b47605a504f8b
Narvana <adhikarijagdish3@gmail.com>
2024-03-27 11:20:47 +0530

    Creating Dashboard Page

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useLocation } from 'react-router-dom'
+import DashSidebar from '../component/DashSidebar'
+import DashProfile from '../component/DashProfile'
 
 function Dashboard() {
+  const location=useLocation()
+  const [tab,setTab] = useState('')
+
+  useEffect(()=>{
+    const urlParams= new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get('tab')
+    if(tabFromUrl)
+    {
+      setTab(tabFromUrl)
+    }
+
+  },[location.search])
+
   return (
-    <div>Dashboard</div>
+    <div>
+      <div className=''> 
+       <DashSidebar/>
+      </div>
+      {tab === 'profile' && <DashProfile/>}
+    </div>
   )
 }
 

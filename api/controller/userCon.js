3217aaca836e52ae7d6f49f52b3ba3065e22b8b1
Narvana <adhikarijagdish3@gmail.com>
2024-03-30 11:09:58 +0530

    Add update user api route

--- api/controller/userCon.js
+++ api/controller/userCon.js
@@ -29,31 +29,38 @@ const userUpdate=async(req,res,next)=>{
     if(req.body.username){
         if(req.body.username.length < 7  || req.body.username.length > 20)
         {
-            return next(errorHandler(400,'Username must be between 7 and 20 characters'))
+            return next(
+                errorHandler(400,'Username must be between 7 and 20 characters')
+            );
         }
-    }
-    if(req.body.username.includes(' ')){
+        if(req.body.username.includes(' '))
+        {
             return next(errorHandler(400,'Username cannot contain spaces')) 
-    }
-    if(req.body.username !== req.body.username.toLowerCase()){
-        return next(errorHandler(400,'Username must be in Lowercase'))
-    }
-    if(!req.body.username.match(/^[a-zA-Z0-9]+$/)){
-       return next(errorHandler(400,'Username can only contain letters and number'))
+        }
+        if(req.body.username !== req.body.username.toLowerCase())
+        {
+            return next(errorHandler(400,'Username must be in Lowercase'))
+        }
+        if(!req.body.username.match(/^[a-zA-Z0-9]+$/))
+        {
+            return next(
+                errorHandler(400,'Username can only contain letters and numbers')
+            );
+        }
     }
     try {
         const updateUser=await Users.findByIdAndUpdate(req.params.userId,{
             $set:{
-                username:req.body.username,
-                email:req.body.email,
-                profilePicture:req.body.profilePicture,
-                password: req.body.password
+                username: req.body.username,
+                email: req.body.email,
+                profilePicture: req.body.profilePicture,
+                password: req.body.password,
             },
         },
         {new: true}
         );
         const {password, ...rest} = updateUser._doc;
-        res.status(200).json(rest);
+        res.json(rest);
     } catch (error) {
         next(error)
     }

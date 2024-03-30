75ccdca64f2912b6793d3f02ff41e23fc13cb7d2
Narvana <adhikarijagdish3@gmail.com>
2024-03-30 11:10:23 +0530

    Update DashSidebar

--- client/src/component/DashSidebar.jsx
+++ client/src/component/DashSidebar.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import {Sidebar} from 'flowbite-react'
 import {HiUser, HiArrowSmRight} from 'react-icons/hi'
-
+import { useSelector } from 'react-redux'
 
 function DashSidebar() {
+    const {currentUser}=useSelector((state)=>state.user)
   return (
     <Sidebar className='w-full md:w-56'>
         <Sidebar.Items> 
             <Sidebar.ItemGroup className='flex flex-col gap-1'>
-                <Sidebar.Item active icon={HiUser} label={'User'} labelColor='dark' as='div'>
+                <Sidebar.Item active icon={HiUser} label={`${currentUser.username}`} labelColor='dark' as='div'>
                     Profile
                 </Sidebar.Item>
                 <Sidebar.Item active icon={HiArrowSmRight} className='cursor-pointer'>
